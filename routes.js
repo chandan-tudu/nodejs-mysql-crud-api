5339f33f3031a2e368f686a750bf3c36b8d8e0c1
Chandan Tudu <chandantudu70@gmail.com>
2024-04-19 15:19:09 +0530

    update and delete method added

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const router = Router({ strict: true });
 router.post("/insert-post", insertPost);
 router.get("/get-all-posts", readPost);
 router.get("/get-post/:post_id", readPost);
-router.post("/update-post/:post_id", updatePost);
-router.post("/delete-post", deletePost);
+router.put("/update-post/:post_id", updatePost);
+router.delete("/delete-post", deletePost);
 
 export default router;
